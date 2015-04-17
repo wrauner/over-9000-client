8794aadf1798568e5f2f3c9d807980c3f9460be4
baborucki <baborucki@gmail.com>
2015-04-17 12:37:54 +0200

    552a757390c0b26ae765cb64 AONT - poprawka transformaty

--- app/src/main/java/com/mkoi/over9000/secure/AllOrNothing.java
+++ app/src/main/java/com/mkoi/over9000/secure/AllOrNothing.java
@@ -2,15 +2,13 @@ package com.mkoi.over9000.secure;
 
 import android.util.Base64;
 
-import org.androidannotations.annotations.EBean;
-
 import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -22,12 +20,11 @@ import javax.crypto.SecretKey;
 /**
  * @Author Bartłomiej Borucki
  */
-
-@EBean
 public class AllOrNothing {
-    List<String> messageblocks= new ArrayList<String>();
-    public void transformMessage(String message) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+    public static ArrayList<String> transformMessage(String message) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         // tablica bajtów wiadomości
+        ArrayList<String> result = new ArrayList<>();
         byte mes[] = message.getBytes();
         //rozmiar bloczka (16B = 128b)
         int blocksize = 16;
@@ -38,13 +35,14 @@ public class AllOrNothing {
         b.putInt(blockcounter);
         byte blockCnt[] = b.array();
         //klucz do szyfrowania licznika
-        Cipher cipherCounter = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        //klucz do szyfrowania XOR(licznik, bloczek)
-        Cipher cipherMessage = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         keyGenerator.init(128);
         //do licznika
         SecretKey secretKey = keyGenerator.generateKey();
+        byte keyBytes[] = secretKey.getEncoded();
+        byte finalBlock[] = new byte[16];
+        System.arraycopy(keyBytes,0,finalBlock,0,keyBytes.length);
         //do bloczka
         Key key = keyGenerator.generateKey();
         for (int i = 0; i<blockcounter; i++) {
@@ -53,28 +51,33 @@ public class AllOrNothing {
             byte[] messagepart;
             messagepart = Arrays.copyOfRange(mes,minrange,maxrange);
             //1. Szyfrowanie licznika
-            cipherCounter.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte encryptedCounter[] = cipherCounter.doFinal(blockCnt);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte encryptedCounter[] = cipher.doFinal(blockCnt);
 
             //2. XORowanie bloczka z zaszyfrowanym licznikiem
             byte xormessage[] = new byte[16];
             for (int j = 0; j < mes.length; j++) {
                 xormessage[j] = (byte) ((int) messagepart[j] ^ (int) encryptedCounter[j]);
             }
+            //3. Obliczanie skrótu wyjścia z 2.
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(xormessage);
+            byte[] hashedBytes = digest.digest();
 
-            //3. XORowanie wyjścia z 2. z licznikiem
-            for (int j = 0; j < blockCnt.length; j++) {
-                xormessage[j] = (byte) ((int) xormessage[j] ^ (int) blockCnt[j]);
+            String messageHash = Base64.encodeToString(hashedBytes, Base64.DEFAULT); //HASH
+            String messageOut = Base64.encodeToString(xormessage, Base64.DEFAULT);  //MESSAGE (m')
+
+            //3. XORowanie klucza z haszami
+            for (int k = 0; k < finalBlock.length; k++){
+                finalBlock[k] = (byte) ((int) finalBlock[k] ^ (int) hashedBytes[k]);
             }
-
-            //4. szyfrownaie wiadomości z wyjściem z 3.
-            cipherMessage.init(Cipher.ENCRYPT_MODE, key);
-            byte encryptedMessage[] = cipherMessage.doFinal(xormessage);
-            messageblocks.add(Base64.encodeToString(encryptedMessage, Base64.DEFAULT));
-            if(i==1){
-
-            }
+            result.add(messageOut);
         }
+        String finalBlockStr = Base64.encodeToString(finalBlock, Base64.DEFAULT);
+        result.add(finalBlockStr);
+
+        return result;
     }
 
     public byte[][] divideArray(byte[] source, int blocksize){
